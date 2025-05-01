0e3907ad3f90bdd8eb5182884d23fdbf84db4ddd
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-05-01 17:48:36 +0100

    commit latest compiled assembly file

---
+++ compiled_assembly.s
@@ -0,0 +1,49 @@
+.section .text
+.global main
+
+main:
+	# Evaluating: 10/5+3*2*3-10
+
+	push $10
+	push $5
+
+	pop %rbx
+	pop %rax
+	cqo				# convert quad rax to oct rdx:rax
+	idiv %rbx		# rax = rdx:rax / rbx
+	push %rax
+
+	push $3
+	push $2
+
+	pop %rbx
+	pop %rax
+	imul %rbx, %rax		# rax = rax * rbx
+	push %rax
+
+	push $3
+
+	pop %rbx
+	pop %rax
+	imul %rbx, %rax		# rax = rax * rbx
+	push %rax
+
+
+	pop %rbx			# right hand operand
+	pop %rax			# left and operand
+	add %rbx, %rax		# rax = rax + rbx
+	push %rax
+
+	push $10
+
+	pop %rbx			# right hand operand
+	pop %rax			# left and operand
+	sub %rbx, %rax		# rax = rax - rbx
+	push %rax
+
+
+	# Final code follows, this takes value from top of stack and places in exit code
+	# Note only the 32 lowest bits of RAX will be used in exit code
+
+	pop %rax
+	ret	# Return from main
