f985236eeb04bced52425b91c696d5485f483f7d
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-05-02 21:11:20 +0100

    add file indices

---
+++ compiled_assembly-1.s
@@ -0,0 +1,20 @@
+.section .text
+.global main
+
+main:
+	# Evaluating: 11+5
+
+	push $11
+	push $5
+
+	pop %rbx			# right hand operand
+	pop %rax			# left hand operand
+	add %rbx, %rax		# rax = rax + rbx
+	push %rax
+
+
+	# Final code follows, this takes value from top of stack and places in exit code
+	# Note only the 32 lowest bits of RAX will be used in exit code
+
+	pop %rax
+	ret	# Return from main
--- controller.c
+++ controller.c
@@ -12,25 +12,28 @@ struct globalCtx ctx;
 // Declaration of the compiler function
 void compiler(struct globalCtx* ctx);
 
-int writeCompiledToFile (char* compiled) {
-    FILE *file = fopen("compiled_assembly.s", "w");
+int writeCompiledToFile (char* compiled, int fileIndex) {
+    char* filename = malloc(20);
+    sprintf(filename, "compiled_assembly-%d.s", fileIndex);
+
+    FILE *file = fopen(filename, "w");
     if (file == NULL) {
         printf("Error opening file for writing.\n");
         return 1;
     }
     fprintf(file, compiled);
     fclose(file);
-    printf("\nCompiled assembly code written to file compiled_assembly.s\n\n");
+    printf("\nCompiled assembly code written to file %s\n\n", filename);
 }
 
-void testCompiler(char* input, int expectedResult) {
+void testCompiler(char* input, int expectedResult, int fileIndex) {
     int result;
     ctx.input = malloc(100);
     strcpy(ctx.input, input);
 
     compiler(&ctx);
 
-    writeCompiledToFile(ctx.compiled);
+    writeCompiledToFile(ctx.compiled, fileIndex);
 
     // Assemble compiled assembly
     system("gcc -o assembled_machine_code.exe compiled_assembly.s");
@@ -50,9 +53,9 @@ void testCompiler(char* input, int expectedResult) {
 int main() {
     ctx.compiled = malloc(1000);
 
-    testCompiler("11+5", 16);
-    testCompiler("10/5+3*2*3-11", 9);
-    testCompiler("10/5+3*2-11+5", 2);
+    testCompiler("11+5", 16, 1);
+    testCompiler("10/5+3*2*3-11", 9, 2);
+    testCompiler("10/5+3*2-11+5", 2, 3);
    
     return 0;
 }
