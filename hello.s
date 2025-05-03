77a28ef3d2814916ce45bcc22bbd1423bd1804e0
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-05-03 10:18:57 +0100

    add sample printf assembly file

---
+++ hello.s
@@ -0,0 +1,22 @@
+.section .data
+msg:
+    .string "Hello, World!\n"
+
+.section .text
+.extern printf
+.globl main
+main:
+    pushq %rbp
+    movq %rsp, %rbp
+
+    subq $32, %rsp            # align stack to 16 bytes
+
+    leaq msg(%rip), %rcx      # Windows x64 ABI: RCX = first argument
+    call printf
+
+    addq $32, %rsp            # restore stack
+
+    movl $0, %eax
+    popq %rbp
+    ret
+    
