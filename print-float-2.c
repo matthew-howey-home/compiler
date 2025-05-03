82c5e342204d2c5b31fa706a00b9e9dd02efd88c
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-05-03 22:08:43 +0100

    new print float with calculated float

---
+++ print-float-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    double x = 2.0 * 3.0;
+    printf("Value: %f\n", x);
+    return 0;
+}
---
+++ print-float-2.s
@@ -0,0 +1,40 @@
+	.file	"print-float-2.c"
+	.text
+	.section .rdata,"dr"
+.LC1:
+	.ascii "Value: %f\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movsd	.LC0(%rip), %xmm0
+	movsd	%xmm0, -8(%rbp)
+	movsd	-8(%rbp), %xmm0
+	movq	-8(%rbp), %rax
+	movapd	%xmm0, %xmm1
+	movq	%rax, %rdx
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+	.align 8
+.LC0:
+	.long	0
+	.long	1075314688
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 14.2.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
