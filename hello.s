6d0e7e412b9bb843acf771a5b1bba412219fafd2
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-05-03 19:53:12 +0100

    printf number

--- hello.s
+++ hello.s
@@ -4,7 +4,7 @@
 
 .section .data
 msg:
-    .string "Hello, World!\n"
+    .string "The number is %d!\n"
 
 .section .text
 .extern printf
@@ -13,6 +13,7 @@ main:
     subq $40, %rsp            # save 32 shadow space + 8 for 64bit alignment
 
     leaq msg(%rip), %rcx      # Windows x64 ABI: RCX = first argument
+    movl $53, %edx            # load int32 53 = second argument 
     call printf
 
     addq $40, %rsp            # restore stack
