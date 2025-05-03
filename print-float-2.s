886f5387fdca6c0915108732da3099b40ccd7758
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-05-03 22:12:39 +0100

    reduced print float code

--- print-float-2.s
+++ print-float-2.s
@@ -5,16 +5,10 @@
 	.ascii "Value: %f\12\0"
 	.text
 	.globl	main
-	.def	main;	.scl	2;	.type	32;	.endef
-	.seh_proc	main
 main:
 	pushq	%rbp
-	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
 	subq	$48, %rsp
-	.seh_stackalloc	48
-	.seh_endprologue
 	call	__main
 	movsd	.LC0(%rip), %xmm0
 	movsd	%xmm0, -8(%rbp)
@@ -29,12 +23,10 @@ main:
 	addq	$48, %rsp
 	popq	%rbp
 	ret
-	.seh_endproc
+
 	.section .rdata,"dr"
 	.align 8
 .LC0:
 	.long	0
 	.long	1075314688
-	.def	__main;	.scl	2;	.type	32;	.endef
-	.ident	"GCC: (Rev2, Built by MSYS2 project) 14.2.0"
-	.def	printf;	.scl	2;	.type	32;	.endef
+	
