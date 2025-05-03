7572aaba4736166a693d84093e2d529842bceec4
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-05-03 20:56:23 +0100

    working code to print float!

---
+++ print-float.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Value: %f", 3.14);
+    return 0;
+}
---
+++ print-float.s
@@ -0,0 +1,33 @@
+	.file	"print-float.c"
+	.text
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Value: %f\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	movabsq	$4614253070214989087, %rax
+	movq	%rax, %xmm0
+	movapd	%xmm0, %xmm1
+	movq	%rax, %rdx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 14.2.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
